7eca122a21fc1a513d1a1f7b4b9cefe8684ed2f5
Henri Lepic <henrilepic@gmail.com>
2026-04-02 09:48:24 +0200

    tp interface 1 solution.

--- tp/interface/1/main.go
+++ tp/interface/1/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	FistName string
 	LastName string
@@ -11,12 +13,32 @@ type Animal struct {
 
 type LuggagesNumb int
 
+type Alien struct {
+	Name string
+}
+
 func main() {
 
 	// Utiliser les structures pour initialiser des éléments
+	var u User = User{FistName: "John", LastName: "Doe"}
+	var a Animal = Animal{Name: "Dog"}
+	var l LuggagesNumb = 3
+	var al Alien = Alien{Name: "Zorg"}
 
-	_ = []interface{} // insérer les éléments créé dans une variable car
+	car := []interface{}{u, a, l, al} // insérer les éléments créé dans une variable car
 
 	// boucler dans les éléments de car
-	// avec le switch type résolvez le type d'origine
+	for _, v := range car {
+		// avec le switch type résolvez le type d'origine
+		switch obj := v.(type) {
+		case User:
+			fmt.Printf("User: %s %s\n", obj.FistName, obj.LastName)
+		case Animal:
+			fmt.Printf("Animal: %s\n", obj.Name)
+		case LuggagesNumb:
+			fmt.Printf("LuggagesNumb: %d\n", obj)
+		default:
+			fmt.Printf("Type inconnu %T , valeur: %v\n", obj, obj)
+		}
+	}
 }
