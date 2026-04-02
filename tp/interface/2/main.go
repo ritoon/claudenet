6e9fde18f03fbba4ac54983a6145ef651f7febba
Henri Lepic <henrilepic@gmail.com>
2026-04-02 09:55:48 +0200

    tp: interface 2 solution.

--- tp/interface/2/main.go
+++ tp/interface/2/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+// Implémenter l'interface fmt.Stringer pour la structure User
+// ne pas retirer cette ligne, elle est utilisée pour vérifier que User implémente bien fmt.Stringer
+var _ fmt.Stringer = User{}
+
+type User struct {
+	FistName string
+	LastName string
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("User: %s %s", u.FistName, u.LastName)
+}
+
+func main() {
+	var u User = User{FistName: "John", LastName: "Doe"}
+	fmt.Println(u) // fmt.Stringer est implémenté pour User, donc la méthode String() est utilisée
 }
