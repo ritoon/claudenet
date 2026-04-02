0cdcbea98c78bd6243e9913dcc0bb60da9a7b668
Henri Lepic <henrilepic@gmail.com>
2026-04-02 13:04:38 +0200

    update gosum

--- tp/slice/3/main.go
+++ tp/slice/3/main.go
@@ -8,16 +8,21 @@ import (
 
 func main() {
 	tbl1 := []int{10, 20, 30}
-	displayInfos("tbl1", &tbl1)
+	displayInfos("tbl1", tbl1)
+
+	tbl2 := make([]int, len(tbl1), 1_000)
+	copy(tbl2, tbl1)
+	displayInfos("tbl2", tbl2)
+
 	// ajouter des éléments dans la slice afin de modifier le tableau sous-jacent de tbl1.
 	// créer une copie de tbl1 vers tbl2 et ajouter des éléments.
 }
 
-func displayInfos(tblName string, tbl *[]int) {
-	hdr := (*reflect.SliceHeader)(unsafe.Pointer(tbl))
+func displayInfos(tblName string, tbl []int) {
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&tbl))
 	p := (*int)(unsafe.Pointer(hdr.Data))
 	msg := fmt.Sprintf("slice %v array pointer %p", tblName, p)
-	msg += fmt.Sprintf(" %v a une capacité %d et une longueur %d", tblName, cap(*tbl), len(*tbl))
+	msg += fmt.Sprintf(" %v a une capacité %d et une longueur %d", tblName, cap(tbl), len(tbl))
 	fmt.Println(msg)
 	fmt.Printf("valeurs de %v : %v\n", tblName, tbl)
 }
